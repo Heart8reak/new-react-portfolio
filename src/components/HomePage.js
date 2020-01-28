86dce1d5592dd647ac7ac96660ed68b67383ba24
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-28 01:01:15 -0500

    styled the project sections mapping over the dataProjects

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,49 +12,60 @@ export default function HomePage() {
             muted="muted"
             loop="loop"
           >
-            <source src="/img/background/bg_vid.mov" type="video/mp4" />
+            <source src="/img/background/bg_vid.mp4" type="video/mp4" />
           </video>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 col-sm-12 text-center">
-              <div className="contents">
-                <h5 className="script-font wow fadeInUp" data-wow-delay="0.2s">
-                  Hi I am
-                </h5>
-                <h2 className="head-title wow fadeInUp" data-wow-delay="0.4s">
-                  Carlos Baez
-                </h2>
-                <p className="script-font wow fadeInUp" data-wow-delay="0.6s">
-                  Front-end Developer
-                </p>
-                <ul className="social-icon wow fadeInUp" data-wow-delay="0.8s">
-                  <li>
-                    <a className="twitter" href="#">
-                      <i className="icon-social-twitter" />
+
+          <div className="container homepage-content">
+            <div className="row">
+              <div className="col-md-12 col-sm-12 text-center">
+                <div className="contents">
+                  <h5
+                    className="script-font wow fadeInUp"
+                    data-wow-delay="0.2s"
+                  >
+                    Hi I am
+                  </h5>
+                  <h2 className="head-title wow fadeInUp" data-wow-delay="0.4s">
+                    Carlos Baez
+                  </h2>
+                  <p className="script-font wow fadeInUp" data-wow-delay="0.6s">
+                    Front-end Developer
+                  </p>
+                  <ul
+                    className="social-icon wow fadeInUp"
+                    data-wow-delay="0.8s"
+                  >
+                    <li>
+                      <a className="twitter" href="#">
+                        <i className="icon-social-twitter" />
+                      </a>
+                    </li>
+                    <li>
+                      <a className="instagram" href="#">
+                        <i className="icon-social-instagram" />
+                      </a>
+                    </li>
+                    <li>
+                      <a className="linkedin" href="#">
+                        <i className="icon-social-linkedin" />
+                      </a>
+                    </li>
+                  </ul>
+                  <div
+                    className="header-button wow fadeInUp"
+                    data-wow-delay="1s"
+                  >
+                    <a className="btn btn-common" href="/projects">
+                      Check out my work
                     </a>
-                  </li>
-                  <li>
-                    <a className="instagram" href="#">
-                      <i className="icon-social-instagram" />
-                    </a>
-                  </li>
-                  <li>
-                    <a className="linkedin" href="#">
-                      <i className="icon-social-linkedin" />
-                    </a>
-                  </li>
-                </ul>
-                <div className="header-button wow fadeInUp" data-wow-delay="1s">
-                  <a className="btn btn-common" href="/projects">
-                    Check out my work
-                  </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       {/* Hero Area End */}
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,17 +4,49 @@ import React, { Component } from 'react';
 export default class Projects extends Component {
   render() {
     let dataProject = this.props.dataProject;
-    return dataProject.map((project, index) => {
-      console.log(dataProject);
-      return (
-        <React.Fragment>
-          <div>
+    // return dataProject.map((project, index) => {
+    console.log(dataProject);
+    return (
+      <React.Fragment>
+        <section id="projects" className="services section-padding">
+          <h2 className="section-title wow flipInX" data-wow-delay="0.4s">
+            What I've done
+          </h2>
+          <div className="container">
+            <div className="row">
+              {/* Services item */}
+              {dataProject.map(project => {
+                return (
+                  <div className="col-md-6 col-lg-3 col-xs-12">
+                    <div
+                      className="services-item wow fadeInDown"
+                      data-wow-delay="0.3s"
+                    >
+                      <div className="icon">
+                        <i className="icon-grid" />
+                      </div>
+                      <div className="services-content">
+                        <h3>
+                          <a href="{project.src}">{project.title}</a>
+                        </h3>
+                        <p>{project.description}</p>
+                      </div>
+                    </div>
+                    <br />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+        ;
+        {/* <div>
             <li>{project.title}</li>
             <p>{project.description}</p>
             <img src={project.src} alt="" />
-          </div>
-        </React.Fragment>
-      );
-    });
+          </div> */}
+      </React.Fragment>
+    );
+    // });
   }
 }
